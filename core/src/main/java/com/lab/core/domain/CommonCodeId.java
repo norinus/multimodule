3edb180f6f4a5fc02d88b910ec51ce456d3bbf7a
klaatus <klaatus@msn.com>
2024-11-09 00:43:13 +0900

    1. 추가 내용: 배치 의존성 추가 2. 작성자 : 황재인 3. 추가일 2024-11-08

--- core/src/main/java/com/lab/core/domain/CommonCodeId.java
+++ core/src/main/java/com/lab/core/domain/CommonCodeId.java
@@ -1,32 +1,15 @@
 package com.lab.core.domain;
 
+import lombok.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Getter
+@Setter
 public class CommonCodeId implements Serializable {
 
     private String commonCode;
+
     private String commonGroupCode;
-
-    public CommonCodeId() {
-    }
-
-    public CommonCodeId(String commonCode, String commonGroupCode) {
-        this.commonCode = commonCode;
-        this.commonGroupCode = commonGroupCode;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CommonCodeId that = (CommonCodeId) o;
-        return Objects.equals(commonCode, that.commonCode) &&
-                Objects.equals(commonGroupCode, that.commonGroupCode);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(commonCode, commonGroupCode);
-    }
 }
