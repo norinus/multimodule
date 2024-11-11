ccc688d4f15e149d585e2ac7c902eb140ece080e
mankatos <klaatus@msn.com>
2024-11-11 16:54:55 +0900

    변경 내용: 사용자 생성 mapper 추가 수정자 : 황재인 수정일: 2024-11-11

--- core/src/main/java/com/lab/core/mapper/member/MemberRoleMapper.java
+++ core/src/main/java/com/lab/core/mapper/member/MemberRoleMapper.java
@@ -1,15 +1,15 @@
 package com.lab.core.mapper.member;
 
-
 import com.lab.core.domain.MemberRole;
 import com.lab.core.dto.member.MemberRoleDTO;
+import com.lab.core.mapper.EntityMapper;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface MemberRoleMapper {
+public interface MemberRoleMapper extends EntityMapper<MemberRoleDTO, MemberRole> {
 
     MemberRoleDTO toDto(MemberRole memberRole);
 
     MemberRole toEntity(MemberRoleDTO memberRoleDTO);
+
 }
